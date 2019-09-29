e5f51710b72696c734083c521256db9382463949
dprageethroshane <dprageethroshane@gmail.com>
2019-09-29 15:00:25 -0700

    Add service projects

---
+++ MyCart.Repository.Contracts/ICartRepository.cs
@@ -0,0 +1,8 @@
+﻿using MyCart.Repository.Models;
+
+namespace MyCart.Repository.Contracts
+{
+    public interface ICartRepository : IRepositoryBase<Cart>
+    {
+    }
+}
---
+++ MyCart.Repository/CartRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyCart.Repository.Contracts;
+using MyCart.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCart.Repository
+{
+    public class CartRepository : RepositoryBase<Cart>, ICartRepository
+    {
+        public CartRepository(MyCartDbContext myCartDbContext)
+            : base(myCartDbContext)
+        {
+        }
+    }
+}
