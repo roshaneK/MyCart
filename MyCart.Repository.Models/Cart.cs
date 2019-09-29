8f554d289083af182a272d33ac42f43a11507b83
dprageethroshane <dprageethroshane@gmail.com>
2019-09-29 14:37:12 -0700

    Configure Repository projects with EF

---
+++ MyCart.Repository.Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCart.Repository.Models
+{
+    public class Cart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
---
+++ MyCart.Repository/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCart.Repository.Contracts;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MyCart.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected MyCartDbContext MyCartDbContext { get; set; }
+
+        public RepositoryBase(MyCartDbContext myCartDbContext)
+        {
+            MyCartDbContext = myCartDbContext;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return MyCartDbContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return MyCartDbContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            MyCartDbContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            MyCartDbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            MyCartDbContext.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            MyCartDbContext.SaveChanges();
+        }
+    }
+}
